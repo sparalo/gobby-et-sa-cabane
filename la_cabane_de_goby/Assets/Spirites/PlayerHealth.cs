13a00053fa9c472fdf48b27f6c4dfe709a016c5f
theo deletang <92048258+sparalo@users.noreply.github.com>
2022-03-31 14:43:57 +0200

    Delete la_cabane_de_goby directory

--- la_cabane_de_goby/Assets/Spirites/PlayerHealth.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PlayerHealth : MonoBehaviour
-{
-    public float Health = 100f; // cet variable est publique et elle represente la santé actuelle du joueur.
-}
